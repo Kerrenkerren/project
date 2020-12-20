304ccbc86d78b0e87222a4aad2cdb5b0b17fd73a
Daria <agy2028@gmail.com>
2020-12-20 12:33:08 +0300

    loops

---
+++ loops-task1.js
@@ -0,0 +1,20 @@
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  private: false
+};
+
+
+for (let i = 0; i < 2; i++) {
+   let movie = prompt('Один из последних просмотренных фильмов?', ''),
+       rating = prompt('На сколько оцените его?', '');
+    personalMovieDB.movies[movie] = rating;
+    personalMovieDB.movies[movie] = rating;
+}
+
+
+console.log(personalMovieDB);

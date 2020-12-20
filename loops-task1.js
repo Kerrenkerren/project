152b52fae2c2d395f76cf950b60ef0a33e80cf5f
Daria <agy2028@gmail.com>
2020-12-20 13:20:18 +0300

    new

--- loops-task1.js
+++ loops-task1.js
@@ -16,5 +16,37 @@ for (let i = 0; i < 2; i++) {
     personalMovieDB.movies[movie] = rating;
 }
 
+let num = 0;
+while (num < 2 ) {
+  let movie = prompt('Один из последних просмотренных фильмов?', ''),
+      rating = prompt('На сколько оцените его?', '');
+  personalMovieDB.movies[movie] = rating;
+  personalMovieDB.movies[movie] = rating;
+  num++;
+}
+
+let num = 0;
+do {
+  let movie = prompt('Один из последних просмотренных фильмов?', ''),
+      rating = prompt('На сколько оцените его?', '');
+  personalMovieDB.movies[movie] = rating;
+  personalMovieDB.movies[movie] = rating;
+  num++;
+} while (num < 2);
+
+
+for (let i = 0; i < 2; i++) {
+  if (i < 1) {
+    console.log('Ololo');
+  } else {
+    console.log('Good');
+  }
+
+  let movie = prompt('Один из последних просмотренных фильмов?', ''),
+      rating = prompt('На сколько оцените его?', '');
+  personalMovieDB.movies[movie] = rating;
+  personalMovieDB.movies[movie] = rating;
+
+}
 
 console.log(personalMovieDB);
